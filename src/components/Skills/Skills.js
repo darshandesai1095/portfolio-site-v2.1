916ed34ace14b43aeb97ad38d04bf68687e5a040
darshandesai1095 <43254178+darshandesai1095@users.noreply.github.com>
2023-04-17 23:46:13 +0100

    add skills component

---
+++ src/components/Skills/Skills.js
@@ -0,0 +1,15 @@
+import React from "react";
+import './Skills.css'
+
+const Skills = () => {
+    return (
+        <div className="skills">
+            <a target="_blank" href="https://icons8.com/icon/20909/html-5">Html 5</a> icon by <a target="_blank" href="https://icons8.com">Icons8</a>
+            <img src="https://icons8.com/icon/20909/html-5"/>
+            <img src="https://icons8.com"/>
+            
+        </div>
+    )
+}
+
+export default Skills
