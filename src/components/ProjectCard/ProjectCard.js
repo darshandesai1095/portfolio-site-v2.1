d7ae5f48a0040ad9a968d5ea65261965c12d69b0
darshandesai1095 <43254178+darshandesai1095@users.noreply.github.com>
2023-04-17 22:42:11 +0100

    project card component

---
+++ src/components/ProjectCard/ProjectCard.js
@@ -0,0 +1,32 @@
+import React, { useState, useEffect } from "react";
+import "./ProjectCard.css"
+import { useDispatch } from 'react-redux'
+import { toggle } from '../../redux/features/GIFPlaybackSlice'
+
+const ProjectCard = (props) => {
+
+    const dispatch = useDispatch()
+
+    return (
+        <div className="project-card" id={`${props.id}`}>
+            <h4 className="project-card__heading">{props.title}</h4>
+            <p className="project-card__text">
+                {props.desc}
+            </p>
+            <div className="project-card__buttons">
+                <button 
+                    className="project-card__gif"
+                    onClick={() => dispatch(toggle(props.gif))}>
+                        See GIF
+                </button>
+                <button 
+                    className="project-card__link-to-site"
+                    onClick={() => window.open(props.site, "_blank", "noopener,noreferrer")}>
+                        Go To Site!
+                </button>
+            </div>
+        </div>
+    )
+}
+
+export default ProjectCard
