cb50350bbddc6582168ce18efe8e12f7bda19b0e
darshandesai1095 <43254178+darshandesai1095@users.noreply.github.com>
2023-04-17 22:44:06 +0100

    add projects page component

---
+++ src/components/ProjectsPage/ProjectsPage.js
@@ -0,0 +1,38 @@
+import React, { useState, useEffect } from 'react';
+import './ProjectsPage.css';
+import ProjectCard from '../ProjectCard/ProjectCard';
+import projects from "../../data/ProjectInfo.js"
+import DemoWindow from '../DemoWindow/DemoWindow';
+import { useSelector } from 'react-redux'
+
+function ProjectsPage() {
+
+    const playbackSelected = useSelector((state) => state.GIFPlayback.value.playback)
+
+    const cards = projects.map( (project,i) => {
+        return (
+            <ProjectCard
+                gif     = {project.gif}
+                altText = {project.alt}
+                desc    = {project.desc}
+                title   = {project.title}
+                react   = {project.react}
+                redux   = {project.redux}
+                router  = {project.router}
+                site    = {project.site}
+                id = {`project-card__${i}`}
+            />
+        )
+    })
+
+  return (
+
+    <div className={`projects-page`}>
+        {cards}
+        {playbackSelected ? <DemoWindow/> : null}
+    
+    </div>
+  );
+}
+
+export default ProjectsPage;
