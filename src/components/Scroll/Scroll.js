c3d0551c9e2aed284a044068d90c7fcf9c836243
darshandesai1095 <43254178+darshandesai1095@users.noreply.github.com>
2023-04-18 19:25:16 +0100

    create scroll component

---
+++ src/components/Scroll/Scroll.js
@@ -0,0 +1,14 @@
+import React, { useState } from "react";
+import "./Scroll.css"
+
+const Scroll = () => {
+
+
+    return (
+        <div className="scroll">
+            
+        </div>
+    )
+}
+
+export default Scroll
