7232586e78b1b2f47565e42a1c85258bb5e068a2
darshandesai1095 <43254178+darshandesai1095@users.noreply.github.com>
2023-04-19 05:59:50 +0100

    update nav icons

--- src/components/NavFrame/NavRight.js
+++ src/components/NavFrame/NavRight.js
@@ -3,30 +3,57 @@ import './NavFrame.css'
 import LinkedInIcon from '@mui/icons-material/LinkedIn';
 import GitHubIcon from '@mui/icons-material/GitHub';
 import ArticleIcon from '@mui/icons-material/Article';
+import Tooltip from '@mui/material/Tooltip';
+// import { Tooltip } from "@mui/material";
 
 const NavRight = () => {
 
     const style = {
-        fill: "#495057",
-        margin: "7px",
+        // fill: "#495057",
+        margin: "5px",
         transform: "scale(1)",
-        opacity: "0.9"
+        opacity: "1"
     }
+
+
 
     return (
         <div className="nav-right">
             <div className='nav-right__icons'>
-                <div className='nav-right__icon'>
-                    <LinkedInIcon style={style}/>
+
+                <div 
+                title="LinkedIn" 
+                id="linkedin" 
+                className='nav-right__icon'
+                onClick={window.open()}
+                >
+                    <LinkedInIcon
+                    style={style} 
+                    id="linkedin__icon"/>
                 </div>
                 
-                <div className='nav-right__icon'>
-                    <GitHubIcon style={{...style, transform: "scale(0.95)"}}/>
+                <div 
+                title="GitHub" 
+                id="github" 
+                className='nav-right__icon'
+                onClick={window.open()} 
+                >
+                    <GitHubIcon 
+                    style={{...style, transform: "scale(0.95)"}} 
+                    id="github__icon" />
                 </div>
                 
-                <div className='nav-right__icon'>
-                    <ArticleIcon style={style}/>
+                <div 
+                title="Résumé" 
+                id="cv" 
+                className='nav-right__icon' 
+                onClick={window.open()}
+                >
+                    <ArticleIcon 
+                    style={style}
+                    id="cv__icon" />
                 </div>
+
             </div>
         </div>
     )
