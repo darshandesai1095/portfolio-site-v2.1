281ad72961994c33ee6e0a34d0c5cb8e5edb2e8a
darshandesai1095 <43254178+darshandesai1095@users.noreply.github.com>
2023-04-19 06:00:27 +0100

    add navigation function

--- src/components/Menu/Menu.js
+++ src/components/Menu/Menu.js
@@ -3,6 +3,7 @@ import MenuBurger from "../MenuBurger/MenuBurger";
 import { useSelector, useDispatch } from 'react-redux'
 import { toggle } from '../../redux/features/GIFPlaybackSlice'
 import "./Menu.css"
+import navigate from "../functions/navigate";
 
 const Menu = () => {
 
@@ -30,26 +31,26 @@ const Menu = () => {
                     // </div>
 
                     <div className="menu__items">
-                        <p className="menu__item hidden">HOMEPAGE</p>
-                        <p className="menu__item hidden">ABOUT</p>
+                        <p className="menu__item hidden" onClick={(e) => navigate(e,"home")}>HOMEPAGE</p>
+                        <p className="menu__item hidden" onClick={(e) => navigate(e,"about")}>ABOUT</p>
                         <p className="menu__item_gif visible" onClick={() => dispatch(toggle(""))}>CLOSE GIF</p>
-                        <p className="menu__item hidden">SKILLS</p>
-                        <p className="menu__item hidden">CONTACT</p>
+                        <p className="menu__item hidden" onClick={(e) => navigate(e,"skills")}>SKILLS</p>
+                        <p className="menu__item hidden" onClick={(e) => navigate(e,"contact")}>CONTACT</p>
                     </div>
 
                     :
 
                     <div className="menu__items">
-                        <p className={`menu__item ${!menuClicked ? "hidden" : "visible"}`}>HOMEPAGE</p>
-                        <p className={`menu__item ${!menuClicked ? "hidden" : "visible"}`}>ABOUT</p>
+                        <p className={`menu__item ${!menuClicked ? "hidden" : "visible"}`} onClick={(e) => navigate(e,"home")}>HOMEPAGE</p>
+                        <p className={`menu__item ${!menuClicked ? "hidden" : "visible"}`} onClick={(e) => navigate(e,"about")}>ABOUT</p>
                         {
                             menuClicked ? 
-                                <p className="menu__item visible">PROJECTS</p> 
+                                <p className="menu__item visible" onClick={(e) => navigate(e,"projects")}>PROJECTS</p> 
                                     : 
                                 <p className="menu__text" onClick={toggleMenu} id="menu__text">MENU</p>
                         }
-                        <p className={`menu__item ${!menuClicked ? "hidden" : "visible"}`}>SKILLS</p>
-                        <p className={`menu__item ${!menuClicked ? "hidden" : "visible"}`}>CONTACT</p>
+                        <p className={`menu__item ${!menuClicked ? "hidden" : "visible"}`} onClick={(e) => navigate(e,"skills")}>SKILLS</p>
+                        <p className={`menu__item ${!menuClicked ? "hidden" : "visible"}`} onClick={(e) => navigate(e,"contact")}>CONTACT</p>
                     </div>
                 }
 
