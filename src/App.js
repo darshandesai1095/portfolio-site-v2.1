478a4713ff66ea0893e01615c322bd841d282b25
darshandesai1095 <43254178+darshandesai1095@users.noreply.github.com>
2023-04-19 06:02:28 +0100

    add intersection observer

--- src/App.js
+++ src/App.js
@@ -5,20 +5,49 @@ import AboutMe from './components/AboutMe/AboutMe';
 import ProjectsPage from './components/ProjectsPage/ProjectsPage';
 import Skills from './components/Skills/Skills';
 import Contact from './components/Contact/Contact';
+import navigate from './components/functions/navigate';
+
 
 function App() {
+
+  const options = {
+    root: null,
+    rootMargin: '0px',
+    threshold: 0.4
+  }
+
+  const setupIntersectionObserver = () => {
+    const observer = new IntersectionObserver((entries) => {
+      entries.forEach(entry => {
+        if (entry.isIntersecting) {
+          // Do something when the element enters the viewport
+          entry.target.classList.add("new")
+
+        } else {
+            entry.target.classList.remove("new")
+        }
+      })
+    }, options)
+  
+    const hiddenElements = document.querySelectorAll(".__hidden__")
+    hiddenElements.forEach(el => observer.observe(el))
+  }
+  
+  window.addEventListener("load", setupIntersectionObserver)
+
+  
   return (
     <div className="App">
       <NavFrame/>
-      <Home/>
-      <AboutMe/>
-      <ProjectsPage/>
-      <Skills/>
-      <Contact/>
+      <section id="home"      className="__hidden__"><Home/></section>
+      <section id="about"     className="__hidden__"><AboutMe/></section>
+      <section id="projects"  className="__hidden__"><ProjectsPage/></section>
+      <section id="skills"    className="__hidden__"><Skills/></section>
+      <section id="contact"   className="__hidden__"><Contact/></section>  
     </div>
   );
 }
 
 export default App;
 
-    /* background-color: #FFE1A4; */
+/* background-color: #FFE1A4; */
