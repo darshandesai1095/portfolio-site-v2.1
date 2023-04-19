586a60cc4eb4036135441e00f75f9dfa0655f644
darshandesai1095 <43254178+darshandesai1095@users.noreply.github.com>
2023-04-19 05:59:39 +0100

    scroll feature

--- src/components/Scroll/Scroll.js
+++ src/components/Scroll/Scroll.js
@@ -1,12 +1,22 @@
 import React, { useState } from "react";
+import SouthSharpIcon from '@mui/icons-material/SouthSharp';
+import ArrowDownwardSharpIcon from '@mui/icons-material/ArrowDownwardSharp';
 import "./Scroll.css"
+import navigate from "../functions/navigate";
 
 const Scroll = () => {
 
+    const style = {
+        fill: "#f8f8f8",
+        transform: "scale(0.8)",
+    }
 
     return (
-        <div className="scroll">
-            
+        <div className="scroll" onClick={(e) => navigate(e, "about")}>
+            <div className="scroll__main">
+                <p className="scroll__text">Scroll</p>
+                <ArrowDownwardSharpIcon style={style}/>
+            </div>
         </div>
     )
 }
