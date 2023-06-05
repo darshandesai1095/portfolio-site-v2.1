1cbf82e6ce7955eb6e472fa65005c7518fc65d6d
darshandesai1095 <43254178+darshandesai1095@users.noreply.github.com>
2023-06-05 18:40:18 +0100

    change scroll opacity onScroll

--- src/components/Scroll/Scroll.js
+++ src/components/Scroll/Scroll.js
@@ -2,8 +2,12 @@ import React from "react";
 import ArrowDownwardSharpIcon from '@mui/icons-material/ArrowDownwardSharp';
 import "./Scroll.css"
 import navigate from "../functions/navigate";
+import { motion, useScroll, useTransform } from "framer-motion";
 
 const Scroll = () => {
+
+    const { scrollYProgress } = useScroll()
+    const opacity = useTransform(scrollYProgress, [0,0.001], [1,0])
 
     const style = {
         fill: "#f8f8f8",
@@ -12,10 +16,10 @@ const Scroll = () => {
 
     return (
         <div className="scroll" onClick={(e) => navigate(e, "about")}>
-            <div className="scroll__main">
+            <motion.div className="scroll__main" style={{opacity}}>
                 <p className="scroll__text">Scroll</p>
                 <ArrowDownwardSharpIcon style={style}/>
-            </div>
+            </motion.div>
         </div>
     )
 }
