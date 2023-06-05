87e0b81399bb90b1cd9cfe1d8b9ba0551f7d9d15
darshandesai1095 <43254178+darshandesai1095@users.noreply.github.com>
2023-06-05 18:25:59 +0100

    update transitions to use framer motion hooks

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,44 +1,50 @@
 import React, { useState, useEffect } from "react";
 import Scroll from "../Scroll/Scroll";
-
 import "./Home.css"
+import { motion, useScroll, useTransform } from "framer-motion";
 
 const Home = () => {
 
-    const [offsetY, setOffsetY] = useState(0);
-    const [innerWidth, setInnerWidth] = useState(window.innerWidth)
-    const [bkgWidth, setBkgWidth] = useState((700 - window.pageYOffset) / 14)
-    const [titleOffsetX, setTitleOffsetX] = useState(window.pageYOffset*1.5 + innerWidth/2.7)
+  const [offsetY, setOffsetY] = useState(0);
+  const [innerWidth, setInnerWidth] = useState(window.innerWidth)
+  const [bkgWidth, setBkgWidth] = useState((700 - window.pageYOffset) / 14)
+  const [titleOffsetX, setTitleOffsetX] = useState(window.pageYOffset*1.5 + innerWidth/2.7)
 
-    useEffect(() => {
-        function handleScroll() {
-          setOffsetY(window.pageYOffset);
-          setInnerWidth(window.innerWidth)
-          setBkgWidth((700 - offsetY) / 14);
-          setTitleOffsetX(window.pageYOffset*1.5+innerWidth/2.7)
-        }
-    
-        window.addEventListener('scroll', handleScroll);
-        window.addEventListener('resize', handleScroll)
-    
-        return () => {
-          window.removeEventListener('scroll', handleScroll)
-          window.removeEventListener('resize', handleScroll)
-        };
-      }, [offsetY, innerWidth]);
+  useEffect(() => {
+      function handleScroll() {
+        setOffsetY(window.pageYOffset)
+        setInnerWidth(window.innerWidth)
+        setBkgWidth((700 - offsetY) / 14)
+        setTitleOffsetX(window.pageYOffset*1.5+innerWidth/2.7)
+      }
+  
+      window.addEventListener('scroll', handleScroll)
+      window.addEventListener('resize', handleScroll)
+  
+      return () => {
+        window.removeEventListener('scroll', handleScroll)
+        window.removeEventListener('resize', handleScroll)
+      }
+    }, [offsetY, innerWidth])
 
-    return (
-        <div className="home">
-            <div className="home__bkg" style={{width:`${bkgWidth}vw`}}></div>
-            <h1 className="home__title" style={{left:`${titleOffsetX}px`}}>
-                HE<br/>LLO<span>.</span>
-            </h1>
-            <Scroll/>
-            <p></p>
+  const { scrollYProgress } = useScroll()
+  const xTitle = useTransform(scrollYProgress, [0, 0.25], [0, innerWidth])
+  const xBkg = useTransform(scrollYProgress, [0, 0.3], [0, -innerWidth])
 
-        </div>
+  return (
+      <div className="home">
+          <motion.div  className="home__bkg" 
+                // style={{width:`${bkgWidth}vw`}}
+                style={{ x: xBkg }}
+          />
+          <motion.h1 className="home__title" style={{ x: xTitle }}>
+              HE<br/>LLO<span>.</span>
+          </motion.h1>
+          <Scroll/>
+          <p></p>
+      </div>
 
-    )
+  )
 }
 
 export default Home
